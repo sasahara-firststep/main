89b8fa51c6ee917e30adea89b3398edf7acf953a
sasahara-firststep <ry.1010.st@gmail.com>
2024-11-28 22:08:43 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,10 +4,17 @@ const navLinks = document.getElementById('nav-links');
 
 // DOMが完全に読み込まれてからスクリプトを実行する
 document.addEventListener('DOMContentLoaded', function() {
-  // ハンバーガーメニューのクリックでメニューを表示・非表示
   if (hamburger && navLinks) {
     hamburger.addEventListener('click', () => {
       navLinks.classList.toggle('active');
+    });
+
+    // ハンバーガーメニューのリンクをクリックしたらメニューを閉じる
+    const navItems = navLinks.querySelectorAll('a');
+    navItems.forEach(item => {
+      item.addEventListener('click', () => {
+        navLinks.classList.remove('active'); // メニューを閉じる
+      });
     });
   }
 });
