ce89cd1d5005d94203156c7ce738cb1b6d44b77c
sasahara-firststep <ry.1010.st@gmail.com>
2024-11-30 10:42:29 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,32 +6,11 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-// スクロール時のアニメーション
-const fadeIns = document.querySelectorAll('.fade-in');
-
-const handleScroll = () => {
-  fadeIns.forEach(element => {
-    if (isInViewport(element)) {
-      element.classList.add('show');
-    }
-  });
-};
-
-const isInViewport = (el) => {
-  const rect = el.getBoundingClientRect();
-  return rect.top >= 0 && rect.bottom <= window.innerHeight;
-};
-
-// ページ読み込み時にアニメーションを開始
-document.addEventListener('DOMContentLoaded', handleScroll);
-
-// スクロールイベントを監視
-window.addEventListener('scroll', handleScroll);
-
 // アクセスカウンター (ローカルストレージ)
 const visitCount = localStorage.getItem('visitCount') ? parseInt(localStorage.getItem('visitCount')) + 1 : 1;
 localStorage.setItem('visitCount', visitCount);
 document.getElementById('visit-counter').innerText = `訪問回数: ${visitCount}`;
+
 
 
 
